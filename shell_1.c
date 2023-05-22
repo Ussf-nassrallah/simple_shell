52528a77a7c355fd7ff4fe7574cbb5d8f797c789
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-05-22 04:15:41 -0700

    Update shell_1.c

--- shell_1.c
+++ shell_1.c
@@ -4,64 +4,64 @@ extern char **environ;
 
 int main()
 {
-	char prompt[] = "#cisfun$ ";
-	char *command[100];
-	char *argv[100];
-	size_t size = 100;
-	int status;
+        char prompt[] = "($) ";
+        char *command;
+        char *argv[100];
+        size_t size = 100;
+        int status;
 
-	while (1)
-	{
-		printf("%s", prompt);
-		if (getline(command, &size, stdin) == -1)
-		{
-			printf("\n");
-			break;
-		}
-		int argc = 0;
-		char *token = strtok(*command, " \t\n");
-		while (token != NULL)
-		{
-			if (strcmp(token, "exit") == 0)
-			{
-				exit(0);
-			}
-			else if (strcmp(token, "env") == 0)
-			{
-				char **env = environ;
-				while (*env)
-				{
-					printf("%s\n", *env);
-					env++;
-				}
-				break;
-			}
-			argv[argc++] = token;
-			token = strtok(NULL, " \t\n");
-		}
-		argv[argc] = NULL;
+        while (1)
+        {
+                printf("%s",prompt);
+                fflush(stdout);
+                if ((command = _getline1()) == NULL)
+                {
+                        printf("\n");
+                        break;
+                }
+                int argc = 0;
+                char *token = strtok(command, " \t\n");
+                while (token != NULL)
+                {
+                        if (strcmp(token, "exit") == 0)
+                        {
+                                exit(0);
+                        }
+                        else if (strcmp(token, "env") == 0)
+                        {
+                                char **env = environ;
+                                while (*env)
+                                {
+                                        printf("%s\n", *env);
+                                        env++;
+                                }
+                                break;
+                        }
+                        argv[argc++] = token;
+                        token = strtok(NULL, " \t\n");
+                }
+                argv[argc] = NULL;
 
-		pid_t pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			int ret = execvp(argv[0], argv);
-			if (ret == -1)
-			{
-				perror(argv[0]);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
-	}
+                pid_t pid = fork();
+                if (pid == -1)
+                {
+                        perror("fork");
+                        exit(EXIT_FAILURE);
+                }
+                else if (pid == 0)
+                {
+                        int ret = execvp(argv[0], argv);
+                        if (ret == -1)
+                        {
+                                perror(argv[0]);
+                                exit(EXIT_FAILURE);
+                        }
+                }
+                else
+                {
+                        waitpid(pid, &status, 0);
+                }
+        }
 
-	return 0;
+        return 0;
 }
-
