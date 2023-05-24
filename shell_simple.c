a73a0ae3dfd81720221c2f900c3ea0ea0e6a0188
riyo3350g <yechouben@gmail.com>
2023-05-24 06:49:16 -0700

    add

--- shell_simple.c
+++ shell_simple.c
@@ -5,44 +5,93 @@
  *
  * Return: Always 0
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	char prompt[] = "#cisfun$ ";
 	char *command[100], *dlm = "\n";
-	char *argv[100], *token;
+	char *input;
+	char *token;
 	size_t size = 100;
 	int status, d, ret;
 	pid_t pid;
 
-	while (1)
+	if (argc == 1)
 	{
-		write(1, prompt, 9);
-		d = getline(command, &size, stdin);
-		if (d == -1)
+		while (1)
 		{
-			perror("\n");
-			break;
-		}
-
-		token = strtok(*command, dlm);
-		argv[0] = token;
-		argv[1] = NULL;
-		pid = fork();
-
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		} else if (pid == 0)
-		{
-			ret = execve(argv[0], argv, NULL);
-			if (ret == -1)
+			write(1, prompt, 9);
+			d = getline(command, &size, stdin);
+			if (d == -1)
 			{
-				perror("./shell");
+				perror("\n");
+				break;
+			}
+
+			token = strtok(*command, dlm);
+			argv[0] = token;
+			argv[1] = NULL;
+			pid = fork();
+
+			if (pid == -1)
+			{
+				perror("fork");
 				exit(EXIT_FAILURE);
 			}
-		} else
-			waitpid(pid, &status, 0);
+			else if (pid == 0)
+			{
+				ret = execve(argv[0], argv, NULL);
+				if (ret == -1)
+				{
+					perror("./shell");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+				waitpid(pid, &status, 0);
+		}
 	}
+	else if (argc == 2)
+	{
+		FILE *fp;
+		input = NULL;
+		size = 0;
+
+		fp = fopen(argv[1], "r");
+		if (fp == NULL)
+		{
+			perror("File opening failed");
+			exit(EXIT_FAILURE);
+		}
+
+		while (getline(&input, &size, fp) != -1)
+		{
+			token = strtok(input, dlm);
+			argv[0] = token;
+			argv[1] = NULL;
+			pid = fork();
+
+			if (pid == -1)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{
+				ret = execve(argv[0], argv, NULL);
+				if (ret == -1)
+				{
+					perror("./shell");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+				waitpid(pid, &status, 0);
+		}
+
+		free(input);
+		fclose(fp);
+	}
+
 	return (0);
 }
+
