9700b85b0d5fcc6709fc4b4ed721707889a76fdc
riyo3350g <yechouben@gmail.com>
2023-05-24 06:20:55 -0700

    betty fix

--- shell_simple.c
+++ shell_simple.c
@@ -1,41 +1,48 @@
 #include "main.h"
 
-int main ()
+/**
+ * main - the starting point for program execution
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-        char prompt[] = "$ ";
-        char *command[100], *dlm = "\n";
-        char *argv[100], *token;
-        size_t size = 100;
-        int status, d;
+	char prompt[] = "#cisfun$ ";
+	char *command[100], *dlm = "\n";
+	char *argv[100], *token;
+	size_t size = 100;
+	int status, d, ret;
 	pid_t pid;
 
-        while(1)
-        {
-		write(1, prompt, 2);
-                d = getline(command, &size, stdin);
-                if(d == -1)
-                {
-                        printf("\n");
-                        break;
-                }
+	while (1)
+	{
+		write(1, prompt, 9);
+		d = getline(command, &size, stdin);
+		if (d == -1)
+		{
+			perror("\n");
+			break;
+		}
 
-                token = strtok(*command, dlm);
-                argv[0] = token;
-                argv[1] = NULL;
-                pid = fork();
-                if (pid == -1) {
-                        perror("fork");
-                        exit(EXIT_FAILURE);
-                } else if (pid == 0) {
-                        int ret = execve(argv[0], argv, NULL);
-                        if (ret == -1) {
-                                perror("./shell");
-                                exit(EXIT_FAILURE);
-                        }
-                } else {
-                        waitpid(pid, &status, 0);
-                }
-        }
+		token = strtok(*command, dlm);
+		argv[0] = token;
+		argv[1] = NULL;
+		pid = fork();
 
-        return (0);
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0)
+		{
+			ret = execve(argv[0], argv, NULL);
+			if (ret == -1)
+			{
+				perror("./shell");
+				exit(EXIT_FAILURE);
+			}
+		} else
+			waitpid(pid, &status, 0);
+	}
+	return (0);
 }
