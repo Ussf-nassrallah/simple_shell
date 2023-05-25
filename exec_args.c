e5b12e2a269c0c3898fd5bd551fcd4a7682a2efc
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-05-25 11:49:14 +0100

    Update exec_args.c

--- exec_args.c
+++ exec_args.c
@@ -8,17 +8,22 @@
  */
 void execArguments(char **arguments, char **env)
 {
-	pid_t pid;
-	int status;
+        pid_t pid;
+        int status;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		execve(arguments[0], arguments, env);
-		exit(0);
-	}
-	else
-	{
-		wait(&status);
-	}
+        pid = fork();
+        if (pid == 0)
+        {
+                int ret = execve(arguments[0], arguments, env);
+                if(ret == -1)
+                {
+                        perror("./shell");
+                        exit(EXIT_SUCCESS);
+                }
+                exit(EXIT_SUCCESS);
+        }
+        else
+        {
+                waitpid(pid, &status, 0);
+        }
 }
