a96dbd948a82f8b64bbd687ae7f73fb49a0484ca
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-05-25 12:55:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,43 +10,50 @@
 
 int main(int argc, char *argv[], char **env)
 {
-        char *command_line = NULL;
-        size_t len = 0;
-        ssize_t read;
-        char **arguments;
-        (void)argc;
-        (void)argv;
+    char *command_line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char **arguments;
+    (void)argc;
+    (void)argv;
 
-        /*initialize_shell();*/
+    /*initialize_shell();*/
 
-        while (1)
+    while (true)
+    {
+        read = getline(&command_line, &len, stdin);
+        trimSpaces(command_line);
+
+        if (_strlen(command_line) == 0 || _strlen(command_line) == 1)
         {
-                read = getline(&command_line, &len, stdin);
-                trimSpaces(command_line);
-
-                if(_strlen(command_line) == 0 || _strlen(command_line) == 1){
-                        exit(EXIT_SUCCESS);
-                }
-
-                if (read == -1)
-                {
-                        exit(EXIT_SUCCESS);
-                }
-                arguments = divider(command_line);
-
-                if (_strcmp(arguments[0], "exit") == 0)
-                {
-                        exit(EXIT_SUCCESS);
-                }
-                else if (_strcmp(arguments[0], "cd") == 0)
-                {
-                        _change_dir(arguments[1]);
-                }
-                else
-                {
-                        execArguments(arguments, env);
-                }
+            free(command_line);
+            exit(EXIT_SUCCESS);
         }
 
-        return (0);
+        if (read == -1)
+        {
+            free(command_line);
+            exit(EXIT_SUCCESS);
+        }
+
+        arguments = divider(command_line);
+
+        if (_strcmp(arguments[0], "exit") == 0)
+        {
+            free(arguments);
+            free(command_line);
+            exit(EXIT_SUCCESS);
+        }
+        else if (_strcmp(arguments[0], "cd") == 0)
+        {
+            _change_dir(arguments[1]);
+            free(arguments);
+        }
+        else
+        {
+            execArguments(arguments, env);
+        }
+    }
+
+    return 0;
 }
