a34a465180fc02e977e1f9d2909472914defde2a
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-05-26 05:46:34 -0700

    create _run function in main.c

--- main.c
+++ main.c
@@ -1,5 +1,49 @@
 #include "main.h"
 
+void _run(char *command_line, ssize_t read, char **env);
+
+/**
+ * _run - run function
+ * @com: command_line
+ * Return: Nothing
+ */
+
+void _run(char *command_line, ssize_t read, char **env)
+{
+	char **arguments;
+
+	if (_strlen(command_line) == 0 || _strlen(command_line) == 1)
+	{
+		free(command_line);
+		exit(EXIT_SUCCESS);
+	}
+	if (read == -1)
+	{
+		free(command_line);
+		exit(EXIT_SUCCESS);
+	}
+	arguments = divider(command_line);
+	if (_strcmp(arguments[0], "exit") == 0)
+	{
+		free(arguments);
+		free(command_line);
+		exit(EXIT_SUCCESS);
+	}
+	else if (_strcmp(arguments[0], "cd") == 0)
+	{
+		_change_dir(arguments[1]);
+		free(arguments);
+	}
+	else if (_strcmp(arguments[0], "env") == 0)
+	{
+		_ptenv(arguments);
+		free(arguments);
+	}
+	else
+		execArguments(arguments, env);
+}
+
+
 /**
  * main - the starting point for program execution
  * @argc: number of arguments
@@ -12,7 +56,6 @@ int main(int argc, char *argv[], char **env)
 	char *command_line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	char **arguments;
 	(void)argc;
 	(void)argv;
 
@@ -20,35 +63,7 @@ int main(int argc, char *argv[], char **env)
 	{
 		read = getline(&command_line, &len, stdin);
 		trimSpaces(command_line);
-		if (_strlen(command_line) == 0 || _strlen(command_line) == 1)
-		{
-			free(command_line);
-			exit(EXIT_SUCCESS);
-		}
-		if (read == -1)
-		{
-			free(command_line);
-			exit(EXIT_SUCCESS);
-		}
-		arguments = divider(command_line);
-		if (_strcmp(arguments[0], "exit") == 0)
-		{
-			free(arguments);
-			free(command_line);
-			exit(EXIT_SUCCESS);
-		}
-		else if (_strcmp(arguments[0], "cd") == 0)
-		{
-			_change_dir(arguments[1]);
-			free(arguments);
-		}
-		else if (_strcmp(arguments[0], "env") == 0)
-		{
-			_ptenv(arguments);
-			free(arguments);
-		}
-		else
-			execArguments(arguments, env);
+		_run(command_line, read, env);
 	}
 	return (0);
 }
